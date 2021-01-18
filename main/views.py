156d3777e544ea16f0e789e7503e70506f7f55b7
Maksatai <erkbvna@gmail.com>
2021-01-18 21:23:33 +0600

    new todoproject

--- main/views.py
+++ main/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render,HttpResponse
 
 def homepage(request):
     return render(request,"index.html")
-
-def test(request):
-    return render(request,"test.html")
-
-def third(request):
-    return render(request,"test3.html")
--- todo/urls.py
+++ todo/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage,test,third
+from main.views import homepage
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",homepage,name="home"),
-    path("test",test,name="test"),
-    path("test3",third,name="test3")
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+    +static(settings.MEDIA_URL,document_root=settings.STATIC_ROOT)
